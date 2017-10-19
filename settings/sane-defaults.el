9b5c84e67e31ee115e5d806174728e3982e276c0
bobrowadam <adam@bigpanda.io>
2017-10-19 23:36:21 +0300

    Better mouse scrolling

--- settings/sane-defaults.el
+++ settings/sane-defaults.el
@@ -119,6 +119,12 @@
  :ensure t
  :config (smooth-scrolling-mode t))
 
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+
 ;; Allow recursive minibuffers
 (setq enable-recursive-minibuffers t)
 
