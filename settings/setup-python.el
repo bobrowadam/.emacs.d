98cfefb759f51c71298eb87d405a3e0ad12d351a
Adam Bobrow <adambobrow@adams-mbp.mynet>
2018-02-11 08:21:02 +0200

    Add python setup

---
+++ settings/setup-python.el
@@ -0,0 +1,25 @@
+;;; package --- Summary
+;;; Commentary:
+;;;; Python setup
+;;; Code:
+
+(use-package jedi
+  :ensure t
+  :init
+  (setq jedi:complete-on-dot t)
+  :hook
+  (python-mode-hook . jedi:setup))
+
+(use-package elpy
+  :ensure t
+  :config
+  (elpy-enable))
+
+(use-package anaconda-mode
+  :disabled t
+  :ensure t
+  :hook
+  (python-mode-hook . anaconda-mode)
+  (python-mode-hook . anaconda-eldoc-mode))
+
+;;; setup-python.el ends here
