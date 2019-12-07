3fc9fa44428f4d03f98e57ad0ace4cffc0c1f192
bobrowadam <adam@bigpanda.io>
2019-12-07 12:04:06 +0200

    Add Vterm commands

--- bob-lisp/shell-defuns.el
+++ bob-lisp/shell-defuns.el
@@ -10,9 +10,8 @@
   (file-name-nondirectory
      (directory-file-name
       (file-name-directory path))))
-(generate-new-buffer "vterm")
 
-(defun bob/vterm-other-window (buffer-name)
+(defun bob/vterm-other-window (&optional buffer-name)
   "Create a new vterm buffer with BUFFER-NAME."
   (let ((buffer buffer-name))
     (unless (get-buffer buffer)
@@ -21,20 +20,29 @@
        (vterm-mode)))
     (pop-to-buffer buffer)))
 
-(defun bob/shell-in-project (&optional user-shell-name)
+(defun bob/vterm (&optional user-shell-name)
   "Open shell on current project with.
 use USER-SHELL-NAME for buffer name"
   (interactive)
-  (let ((shell-name (format "*SHELL*::%s" (upcase (get-dir-name
-                                                   (or (projectile-project-root)
-                                                       default-directory))))))
+  (let ((shell-name (format "*SHELL*::%s" (upcase default-directory))))
     (bob/vterm-other-window shell-name)))
+
+(defun bob/projectile-run-vterm ()
+  "Invoke `shell' in the project's root.
+
+Switch to the project specific shell buffer if it already exists."
+  (interactive)
+  (if (projectile-project-p)
+      (projectile-with-default-dir (projectile-ensure-project (projectile-project-root))
+        (bob/vterm-other-window (concat "*SHELL " (projectile-project-name) "*")))
+    (vterm)))
 
 (defun bob/jump-to-shell ()
   "Jump to a shell buffer."
   (interactive)
   (if-let* ((shell-buffers (seq-filter
-                            (lambda (b) (or (s-contains\? "*SHELL*" b  t)
+                            (lambda (b) (or (s-contains\? "*shell*" b  t)
+                                            (s-contains\? "*shell" b  t)
                                             (s-contains\? "*eshell" b  t)
                                             (s-contains\? "*sbt*" b  t)
                                             (s-contains\? "vterm<" b  t)))
