a10df37dc2d6f3a01069506bcb026d547b57b9fe
Adam Bobrow <bob@Adams-MacBook-Pro.local>
2020-02-29 15:08:35 +0200

    Made jump to shell aware on last and current shell buffers

--- bob-lisp/shell-defuns.el
+++ bob-lisp/shell-defuns.el
@@ -29,28 +29,46 @@ use USER-SHELL-NAME for buffer name"
 
 (defun bob/projectile-run-vterm (&optional arg)
   "Invoke `shell' in the project's root.
-
-Switch to the project specific shell buffer if it already exists."
+Switch to the project specific shell buffer if it already exists.
+Passing a prefix arg 0 will allow adding a custom suffix to the shell buffer name.
+Any other prefis will be used as the suffix itself."
   (interactive "P")
-  (if (projectile-project-p)
-      (projectile-with-default-dir (projectile-ensure-project (projectile-project-root))
-        (bob/vterm-other-window (concat "*SHELL " (projectile-project-name) "*" (to-string arg))))
-    (vterm)))
+  (-let [suffix (cond ((eq arg 0) (read-string "enter custom suffix: "))
+                      (arg (to-string arg))
+                      (t ""))]
+   (if (projectile-project-p)
+       (projectile-with-default-dir (projectile-ensure-project (projectile-project-root))
+         (bob/vterm-other-window (concat "*SHELL " (projectile-project-name) "* " suffix)))
+     (vterm))))
 
+(defvar bob/last-shell-buffer nil)
 (defun bob/jump-to-shell ()
   "Jump to a shell buffer."
   (interactive)
-  (if-let* ((shell-buffers (seq-filter
-                            (lambda (b) (or (s-contains\? "*shell*" b  t)
-                                            (s-contains\? "*shell" b  t)
-                                            (s-contains\? "*eshell" b  t)
-                                            (s-contains\? "*sbt*" b  t)
-                                            (s-contains\? "vterm<" b  t)))
-                            (mapcar (function buffer-name) (buffer-list))))
+  (if-let* ((shell-buffers (bob/drop-current-shell-buffer
+                            (set-last-shell-buffer-as-first
+                             (seq-filter
+                              (lambda (b) (or (s-contains\? "*shell*" b  t)
+                                              (s-contains\? "*shell" b  t)
+                                              (s-contains\? "*eshell" b  t)
+                                              (s-contains\? "*sbt*" b  t)
+                                              (s-contains\? "vterm<" b  t)))
+                              (mapcar (function buffer-name) (buffer-list))))))
             (shell-buffer (ivy-completing-read "Shell: " shell-buffers)))
-      (switch-to-buffer shell-buffer)
+      (progn
+        (setq bob/last-shell-buffer shell-buffer)
+       (switch-to-buffer shell-buffer))
     (message "No Shell bufers exists")))
 
+(defun bob/drop-current-shell-buffer (buffers)
+  (seq-remove (lambda (buf) (cond
+                             ((equal buf (buffer-name)) t)))
+              buffers))
+
+(defun set-last-shell-buffer-as-first (buffers)
+  (seq-sort (lambda (a b)
+              (equal a bob/last-shell-buffer))
+            buffers))
 
 (provide 'shell-defuns)
 ;;; shell-defuns.el ends here
