6fd25b118e7e23180a770a68b5531a99b0108cf8
bob <bobrow.adam@gmail.com>
2020-04-11 17:19:31 +0300

    Add dired-aux

--- bob-lisp/setup-dired.el
+++ bob-lisp/setup-dired.el
@@ -3,4 +3,15 @@
 (setq dired-use-ls-dired nil)
 (setq dired-listing-switches "-alh")
 
+(use-package dired-aux
+  :ensure nil
+  :config
+  (setq dired-isearch-filenames 'dwim)
+  ;; The following variables were introduced in Emacs 27.1
+  (setq dired-create-destination-dirs 'ask)
+  (setq dired-vc-rename-file t)
+  :bind (:map dired-mode-map
+              ("C-+" . dired-create-empty-file)
+              ("M-s f" . nil)))
+
 (provide 'setup-dired)
