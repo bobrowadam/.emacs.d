9b166544c3f8c87337151c5a854d974bdcaa3e26
bob <bobrow.adam@gmail.com>
2020-04-21 06:55:11 +0300

    Fix setup-parens

--- bob-lisp/setup-parens.el
+++ bob-lisp/setup-parens.el
@@ -1,21 +1,14 @@
 (use-package paredit
-  :demand t
   :hook
-  (emacs-lisp-mode . (lambda () (progn
-                                  (smartparens-mode -1)
-                                  (paredit-mode))))
-  (eval-expression-minibuffer-setup . (paredit-mode))
-  (racket-mode . paredit-mode))
+  (eval-expression-minibuffer-setup . enable-paredit-mode))
 
 (use-package smartparens
   :demand t
-  :config
+  :init
   (setq sp-ignore-modes-list '(minibuffer-inactive-mode emacs-lisp-mode eval-expression-minibuffer-setup))
-  (show-smartparens-global-mode t)
-  (smartparens-global-mode t)
-  (sp-local-pair 'js2-mode "{ " " }" :trigger-wrap "{")
-  :hook
-  (comint-mode . smartparens-mode)
-  (eshell-mode . smartparens-mode))
+  :config
+  (require 'smartparens-config)
+  (smartparens-global-strict-mode t)
+  (sp-local-pair 'js2-mode "{ " " }" :trigger-wrap "{"))
 
 (provide 'setup-parens)
