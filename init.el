eb3e77f0a338b3514fca7203ff1b2c62a8a1d5af
Adam Bobrtow <bob@Adams-MacBook-Pro.local>
2020-05-03 07:33:06 +0300

    Speed up init by increaseing GC memory on init phase only

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+(setq gc-cons-threshold (* 50 1000 1000))
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)
+            (setq gc-cons-threshold (* 2 1000 1000))))
+
 (require 'package)
 (setq debug-on-error nil)
 (setq package-enable-at-startup nil)
