6085f9983b80db34552ea1fa5b3f7730fea5bc6a
Adam Bobrow <adam@riseup.co.il>
2020-05-12 22:31:22 +0300

    Add dired-subtree

--- bob-lisp/setup-dired.el
+++ bob-lisp/setup-dired.el
@@ -1,17 +1,31 @@
-(require 'dired)
-(require 'dired-x)
-(setq dired-use-ls-dired nil)
-(setq dired-listing-switches "-alh")
+(use-package dired
+  :config
+  (setq dired-use-ls-dired nil)
+  (setq dired-listing-switches "-alh")
+  :ensure nil
+  )
+
+(use-package dired-x
+  :ensure nil
+  :demand t
+  )
 
 (use-package dired-aux
+  :demand t
   :ensure nil
   :config
   (setq dired-isearch-filenames 'dwim)
-  ;; The following variables were introduced in Emacs 27.1
   (setq dired-create-destination-dirs 'ask)
   (setq dired-vc-rename-file t)
   :bind (:map dired-mode-map
               ("C-+" . dired-create-empty-file)
               ("M-s f" . nil)))
+
+(use-package dired-subtree
+  :ensure t
+  :bind (:map dired-mode-map
+              ("<tab>" . dired-subtree-toggle)
+              ("<C-tab>" . dired-subtree-cycle)
+              ("<backtab>" . dired-subtree-remove)))
 
 (provide 'setup-dired)
