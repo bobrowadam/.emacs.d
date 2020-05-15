87262b89d81bcb2de69cb894c51d16ce30354bf6
Adam Bobrow <adam@riseup.co.il>
2020-05-15 15:01:51 +0300

    Make clean the default init file

--- init.el
+++ init.el
@@ -1,364 +1,107 @@
-(setq gc-cons-percentage 0.9)
+(require 'org)
 
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (message "Emacs ready in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)
-            (setq gc-cons-percentage 0.1)))
+(setq-default indent-tabs-mode nil)
+(setq org-display-inline-images t)
+(setq org-redisplay-inline-images t)
+(setq org-startup-with-inline-images "inlineimages")
 
-(require 'package)
-(setq debug-on-error nil)
-(setq package-enable-at-startup nil)
+(setq default-frame-alist
+      (append (list '(width . 72) '(height . 40))))
 
-(unless package--initialized (package-initialize))
+(setq org-confirm-elisp-link-function nil)
+      
+				  
+(global-set-key [(control z)]         'undo)
+(global-set-key "\C-x\C-x"            'execute-extended-command)
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(unless (not (file-exists-p custom-file))
-  (load custom-file))
+(set-frame-font "Roboto Mono Light 18")
 
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")))
+(set-frame-parameter (selected-frame) 'internal-border-width 20)
+(setq x-underline-at-descent-line t)
+(setq initial-major-mode 'text-mode)
+(setq-default line-spacing 0)
+(set-default 'cursor-type  '(hbar . 2))
+(blink-cursor-mode 0)
+(fringe-mode '(0 . 0))
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(setq frame-background-mode 'light)
+(set-background-color "#ffffff")
+(set-foreground-color "#666666")
 
-(eval-when-compile
-  (require 'use-package))
-(savehist-mode)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message t)
+(setq inhibit-startup-message t)   ;; Show/hide startup page
+(setq initial-scratch-message nil) ;; Show/hide *scratch* buffer message
+;; (menu-bar-mode 0)                  ;; Show/hide menubar
+(tool-bar-mode 0)                  ;; Show/hide toolbar
+(tooltip-mode  0)                  ;; Show/hide tooltip
+(scroll-bar-mode 0)                ;; Show/hide scrollbar
 
-(use-package use-package-chords
-  :ensure t
-  :config (key-chord-mode 1))
 
-(setq use-package-always-defer t)
-(setq use-package-always-ensure t)
-(setq use-package-verbose t)
-(setq use-package-debug t)
-(setq use-package-compute-statistics t)
 
-(use-package dash)
-(use-package dap-mode :disabled t)
-(use-package setup-dired
-  :load-path "./bob-lisp"
-  :demand t
-  :hook (dired-mode . auto-revert-mode))
+(defun mode-line-render (left right)
+  "Return a string of `window-width' length containing left, and
+   right aligned respectively."
+  (let* ((available-width (- (window-total-width) (length left) )))
+    (format (format "%%s %%%ds" available-width) left right)))
 
-(use-package saveplace
-  :config
-  (setq save-place-file "~/.emacs.d/saveplace")
-  (setq save-place-forget-unreadable-files t)
-  (save-place-mode 1))
 
-(use-package f)
-(use-package server
-  :init (server-start))
+(setq-default header-line-format
+  '(:eval (mode-line-render
 
-(use-package which-key
-  :demand t
-  :if (window-system)
-  :config
-  (which-key-mode 1))
+   (format-mode-line
+    (list
+     (propertize "File " 'face `(:weight regular))
+     "%b "
+     '(:eval (if (and buffer-file-name (buffer-modified-p))
+         (propertize "(modified)" 
+		     'face `(:weight light
+			     :foreground "#aaaaaa"))))))
+   
+   (format-mode-line
+    (propertize "%3l:%2c "
+	'face `(:weight light :foreground "#aaaaaa"))))))
 
-(use-package exec-path-from-shell
-  :if (window-system)
-  :demand t
-  :init (setq exec-path-from-shell-arguments '("-l"))
-  :config (exec-path-from-shell-initialize))
+(set-face-attribute 'region nil
+		    :background "#f0f0f0")
+(set-face-attribute 'highlight nil
+		    :foreground "black"
+		    :background "#f0f0f0")
+(set-face-attribute 'org-level-1 nil
+		    :foreground "black"
+		    :weight 'regular)
+(set-face-attribute 'org-link nil
+		    :underline nil
+		    :foreground "dark blue")
+(set-face-attribute 'org-verbatim nil
+		    :foreground "dark blue")
+(set-face-attribute 'bold nil
+ 		    :foreground "black"
+		    :weight 'regular)
 
-(use-package flycheck)
 
-(use-package basic-settings
-  :demand t
-  :load-path "./bob-lisp"
-  :bind
-  ("C-c M-s" . isearch-forward-symbol-at-point)
-  ("M-i" . imenu)
-  ("C-x j" . whitespace-cleanup)
-  ("C-^" . (lambda () (interactive (delete-indentation -1))))
-  ("M-C-h" . backward-kill-sexp)
-  ("C-x -" . my/gloden-ratio)
-  ("C-x f" . recentf-open-files)
-  ("M-o" . other-frame)
-  ("C-x k" . kill-this-buffer)
-  ("M-SPC" . cycle-spacing)
-  :chords
-  (("jj" . "_")
-   ("ii" . "|")
-   ("qq" . "~")))
+(setq-default mode-line-format   "")
 
-(use-package setup-eshell
-  :disabled
-  :if (window-system)
-  :demand t
-  :load-path "./bob-list")
+(set-face-attribute 'header-line nil
+;;                    :weight 'regular
+		    :height 140
+                    :underline "black"
+                    :foreground "black"
+		    :background "white"
+                    :box `(:line-width 3 :color "white" :style nil))
+(set-face-attribute 'mode-line nil
+                    :height 10
+                    :underline "black"
+                    :background "white"
+		                :foreground "white"
+                    :box nil)
+(set-face-attribute 'mode-line-inactive nil
+                    :box nil
+                    :inherit 'mode-line)
+(set-face-attribute 'mode-line-buffer-id nil 
+                    :weight 'light)
+(setq org-hide-emphasis-markers t)
 
-(use-package tramp-settings
-  :demand t
-  :if (window-system)
-  :load-path "./bob-lisp")
-
-(use-package remote-defuns
-  :load-path "./bob-lisp")
-
-(use-package misc-funcs
-  :if (window-system)
-  :demand t
-  :load-path "./bob-listp"
-  :bind
-  ("C-+" . increment-number-at-point)
-  ("C-_" . decrement-number-at-point)
-)
-
-(use-package edit-funcs
-  :if (window-system)
-  :demand t
-  :load-path "./bob-lisp"
-  :bind
-  ("C-`" . unpop-to-mark-command)
-  ("M-`" . jump-to-mark))
-
-(use-package setup-parens
-  :if (window-system)
-  :load-path "./bob-listp"
-  :demand t
-  :bind (:map smartparens-mode-map
-              ("M-(" . sp-wrap-round)
-              ("M-s" . sp-unwrap-sexp)
-              ("C-)" . sp-forward-slurp-sexp)
-              ("C-}" . sp-forward-barf-sexp)
-              ("C-{" . sp-backward-barf-sexp)
-              ("C-(" . sp-backward-slurp-sexp)
-              ("C-'" . sp-rewrap-sexp)
-              ("M-S" . sp-split-sexp)
-              ("M-J" . sp-join-sexp)
-              ("M-W" . sp-copy-sexp)))
-
-(use-package inf-mongo)
-
-(use-package ibuffer-setup
-  :demand
-  :load-path "./bob-lisp")
-
-(use-package setup-ivy
-  :demand t
-  :load-path "./bob-lisp"
-  :bind
-  (("M-x" . counsel-M-x)
-   ("C-x C-f" . counsel-find-file)
-   ("C-x C-m" . counsel-mark-ring)
-   ("C-c C-s C-r" . counsel-rg)
-   ("C-c C-s C-s" . swiper)))
-
-(use-package setup-magit
-  :if (window-system)
-  :bind
-  ("C-x g" . magit-status)
-  :demand t
-  :load-path "./bob-lisp")
-
-(use-package whole-line-or-region
-  :demand t
-  :init (whole-line-or-region-mode 1))
-
-(use-package company
-  :if (window-system)
-  :demand t
-  :init
-  (setq company-tooltip-align-annotations t)
-  (setq company-minimum-prefix-length 1)
-  (setq company-idle-delay 0.2)
-  :config (global-company-mode 1))
-
-(use-package setup-projectile
-  :if (window-system)
-  :load-path "./bob-lisp"
-  :demand t
-  :bind
-  ("C-c e" . my-run-eshell))
-
-(use-package sicp)
-
-(use-package ace-window
-  :bind ( "C-x o" . ace-window)
-  :config
-  (setq aw-scope 'frame)
-  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
-
-(use-package highlight-indent-guides :if (window-system))
-
-(use-package lsp-setup
-  :if (window-system)
-  :demand t
-  :load-path "./bob-lisp")
-
-(use-package scala-setup
-  :disabled
-  :if (window-system)
-  :after (company lsp-setup)
-  :demand t
-  :load-path "./bob-lisp")
-
-(use-package setup-js
-  :if (window-system)
-  :demand t
-  :load-path "./bob-lisp")
-
-(use-package haskell-setup
-  :disabled
-  :if (window-system)
-  :demand t
-  :load-path "./bob-lisp")
-
-(use-package org-setup
-  :if (window-system)
-  :demand t
-  :load-path "./bob-lisp"
-  :bind
-  ("C-c a" . org-agenda)
-  ("C-c c" . org-capture)
-  ("C-c S" . org-save-all-org-buffers)
-  ("C-c r" . #'my/refresh-google-calendar)
-  ("C-c v" . org-brain-visualize)
-  ("C-c l" . org-store-link))
-
-(use-package restclient
-  :init
-  (add-to-list 'auto-mode-alist '("\\.client$" . restclient-mode))
-  :if (window-system))
-
-(use-package avy
-  :init (setq avy-case-fold-search nil)
-  :bind
-  ("C-c M-d" . avy-goto-char-in-line)
-  ("C-c M-c" . avy-goto-word-1))
-
-(use-package ace-jump-mode
-  :disabled
-  :init
-  (setq ace-jump-mode-case-fold nil)
-  :demand
-  :bind
-  ("C-c M-c" . ace-jump-mode))
-
-(use-package ripgrep
-  :init (setq wgrep-auto-save-buffer t)
-  :bind
-  ("C-c M-r" . ripgrep-regexp)
-  (:map ripgrep-search-mode-map ("C-x C-q" . ivy-wgrep-change-to-wgrep-mode)))
-
-(use-package deadgrep
-  :bind ("C-c C-d" . deadgrep))
-
-(use-package wgrep)
-
-(use-package setup-snippets
-  :if (window-system)
-  :demand t
-  :bind ("C-c TAB" . yas-expand)
-  :load-path "./bob-lisp")
-
-(use-package expand-region
-  :bind ("M-#" . er/expand-region))
-
-(use-package anzu
-  :if (window-system)
-  :defer 10
-  :demand t
-  :bind (("C-M-%" . anzu-query-replace-regexp)
-         ("M-%" . anzu-query-replace))
-  :config
-  (global-anzu-mode 1))
-
-(use-package multiple-cursors
-  :if (window-system)
-  :bind
-  (("C->" . 'mc/mark-next-like-this)
-   ("C-<" . 'mc/mark-previous-like-this)))
-
-(use-package setup-rust
-  :if (window-system)
-  :after (lsp-setup)
-  :demand t
-  :bind (:map rust-mode-map
-              ("C-c C-c C-r" . rust-run)
-              ("C-c C-c C-b" . rust-compile)
-              ("C-c C-c C-s". sbt-switch-to-active-sbt-buffer))
-  :load-path "./bob-lisp")
-
-(use-package kubernetes
-  :disabled
-  :commands (kubernetes-overview)
-  :init
-  :hook (kubernetes-overview-mode . (lambda () (setenv "AWS_PROFILE" "dev-k8s")))
-  :config
-  (setq kubernetes-poll-frequency 30)
-  (setq kubernetes-redraw-frequency 30)
-  :bind
-  ("C-c k" . kubernetes-overview))
-
-(use-package zoom-window
-  :init
-  (custom-set-variables
-   '(zoom-window-mode-line-color "DarkBlue"))
-  :bind ("C-x C-z" . zoom-window-zoom))
-
-(use-package control-mode
-  :if (window-system)
-  :init (control-mode-default-setup))
-
-(use-package elpy
-  :disabled
-  :if (window-system)
-  :init
-  (advice-add 'python-mode :before 'elpy-enable))
-
-(use-package try)
-
-(use-package shell-defuns :load-path "./bob-lisp" :demand t)
-(use-package vterm
-  :if (window-system)
-  :after shell-defuns
-  :demand t
-  :config
-  (define-key vterm-mode-map [remap whole-line-or-region-yank] #'vterm-yank)
-  :bind
-  ("C-c s s". bob/projectile-run-vterm)
-  ("C-c s e" . bob/vterm)
-  ("C-c s j" . bob/jump-to-shell)
-  (:map vterm-mode-map ("C-c C-j" . vterm-copy-mode))
-  (:map vterm-copy-mode-map ("C-c C-j" . vterm-copy-mode)))
-
-(use-package setup-web-dev
-  :demand t
-  :load-path "./bob-lisp")
-
-(use-package perspective
-  :disabled
-  :demand t
-  :bind
-  ("C-x b" . persp-ivy-switch-buffer)
-  ("C-x C-b" . persp-ibuffer)
-  :hook (kill-emacs . persp-state-save)
-  :config
-  (setq persp-state-default-file (concat user-emacs-directory ".persp-state"))
-  (persp-state-load persp-state-default-file))
-
-(use-package yaml-mode)
-(use-package markdown-mode
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "pandoc"))
-
-(put 'narrow-to-region 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
-(put 'set-goal-column 'disabled nil)
-
-;;; init.el ends here
+;; Delete with C-h;
+(define-key key-translation-map [?\C-h] [?\C-?])
+(define-key key-translation-map (kbd "<f1>") (kbd "C-h"))
