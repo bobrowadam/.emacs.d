a1b045d67da08cd360b654a1d0fcdb9df3c63a1a
Adam Bobrow <adam@riseup.co.il>
2020-09-20 23:37:22 +0300

    Use new ivy config

--- bob-lisp/setup-ivy.el
+++ bob-lisp/setup-ivy.el
@@ -11,32 +11,64 @@
   :config
   (amx-mode 1))
 
+;; (use-package ivy
+;;   :demand t
+;;   :config
+;;   (setq ivy-re-builders-alist
+;;         '((counsel-rg . ivy--regex-plus)
+;;           (t . ivy--regex-fuzzy)
+;;           ;; (t . ivy-prescient-re-builder)
+;;           ))
+;;   (setq ivy-use-selectable-prompt t)
+;;   (setq ivy-initial-inputs-alist nil)
+;;   (setq ivy-use-virtual-buffers t)
+;;   (setq ivy-count-format "(%d%d) ")
+;;   (ivy-mode 1))
+
 (use-package ivy
-  :demand t
+  :commands ivy-mode
+  :hook (after-init . ivy-mode)
+  :delight ivy-mode
+  :bind (:map ivy-minibuffer-map
+              ("C-j" . ivy-immediate-done)
+              ("RET" . ivy-alt-done))
   :config
-  (setq ivy-re-builders-alist
-        '((t . ivy--regex-fuzzy)))
-  (setq ivy-use-selectable-prompt t)
-  (setq ivy-initial-inputs-alist nil)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d%d) ")
-  (ivy-mode 1))
+  (ivy-prescient-mode t)
+  (setq ivy-use-virtual-buffers t
+        ivy-virtual-abbreviate 'full
+        ivy-on-del-error-function nil
+        ivy-use-selectable-prompt t
+        enable-recursive-minibuffers t
+        ivy-re-builders-alist
+        '((swiper . ivy--regex-plus)
+          (swiper-isearch . ivy--regex-plus)
+          (counsel-ag . ivy--regex-plus)
+          (counsel-rg . ivy--regex-plus)
+          (t . ivy-prescient-re-builder))))
+
+(use-package ivy-prescient
+  :commands ivy-prescient-mode
+  :custom
+  (prescient-filter-method '(literal regexp initialism fuzzy))
+  (ivy-prescient-retain-classic-highlighting t)
+  :config
+  (prescient-persist-mode t))
 
 (use-package ivy-posframe
   :disabled t
   :demand t
   :config
   (setq ivy-posframe-display-functions-alist
-      '((swiper          . ivy-posframe-display-at-point)
-        (complete-symbol . ivy-posframe-display-at-point)
-        (counsel-M-x     . ivy-posframe-display-at-window-bottom-left)
-        (counsel-find-file . ivy-posframe-display-at-window-bottom-left)
-        (ivy-switch-buffer . ivy-posframe-display-at-frame-center)
-        (projectile-switch-to-buffer . ivy-posframe-display-at-frame-center)
-        (t               . ivy-posframe-display-at-frame-center)))
+        '((swiper          . ivy-posframe-display-at-point)
+          (complete-symbol . ivy-posframe-display-at-point)
+          (counsel-M-x     . ivy-posframe-display-at-window-bottom-left)
+          (counsel-find-file . ivy-posframe-display-at-window-bottom-left)
+          (ivy-switch-buffer . ivy-posframe-display-at-frame-center)
+          (projectile-switch-to-buffer . ivy-posframe-display-at-frame-center)
+          (t               . ivy-posframe-display-at-frame-center)))
   (setq ivy-posframe-parameters
-      '((left-fringe . 8)
-        (right-fringe . 8)))
+        '((left-fringe . 8)
+          (right-fringe . 8)))
   (ivy-posframe-mode 1))
 
 (use-package counsel
