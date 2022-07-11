a70900ac60851edb20356500bc98760d8acd2f3d
Adam Bobrow <adam@riseup.co.il>
2022-07-11 09:41:53 +0300

    Use pixel precision

--- init.el
+++ init.el
@@ -90,18 +90,7 @@
 (setq initial-scratch-message ";; Oh it's you again")
 
 ;; Vertical Scroll
-;; (setq scroll-step 1)
-;; (setq scroll-margin 1)
-(setq scroll-conservatively 101)
-(setq scroll-up-aggressively 0.01)
-(setq scroll-down-aggressively 0.01)
-(setq auto-window-vscroll nil)
-(setq fast-but-imprecise-scrolling nil)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-(setq mouse-wheel-progressive-speed nil)
-;; Horizontal Scroll
-(setq hscroll-step 1)
-(setq hscroll-margin 1)
+(pixel-scroll-precision-mode)
 (setq display-time-day-and-date t)
 (setq display-time-24hr-format t)
 (setq display-time-default-load-average nil)
