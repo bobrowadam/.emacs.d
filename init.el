93349661b0edee837ce8ea7ae23ba5044dba1bee
Adam Bobrow <adam@riseup.co.il>
2022-07-11 09:50:22 +0300

    Stop native compile

--- init.el
+++ init.el
@@ -39,7 +39,7 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(setq package-native-compile t)
+(setq package-native-compile nil)
 (setq use-package-always-defer t)
 (setq use-package-always-ensure t)
 (setq use-package-verbose t)
