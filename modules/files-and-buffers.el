cd382b8f7c4eacc21bc4ccc956f76cf3af05111f
Adam Bobrow <adam@riseup.co.il>
2022-07-23 15:45:58 +0300

    Use dirvishh

--- modules/files-and-buffers.el
+++ modules/files-and-buffers.el
@@ -6,42 +6,19 @@
   :hook (dired-mode . (lambda () (dired-hide-details-mode 1)))
   :ensure nil)
 
-(use-package dired-x :ensure nil :defer 1)
-(use-package dired-aux
-  :ensure nil
-  :after (dired)
-  :config
-  (setq dired-isearch-filenames 'dwim)
-  (setq dired-create-destination-dirs 'ask)
-  (setq dired-vc-rename-file t)
-  :bind (:map dired-mode-map
-              ("C-+" . dired-create-empty-file)
-              ("M-s f" . nil)))
-
-(use-package dired-subtree
-  :after (dired)
-  :ensure t
-  :bind (:map dired-mode-map
-              ("<tab>" . dired-subtree-toggle)
-              ("<C-tab>" . dired-subtree-cycle)
-              ("<backtab>" . dired-subtree-remove)))
-
-(use-package diredfl
-  :ensure t
-  :init (diredfl-global-mode))
-
 (use-package dired-rsync
   :init
   (setq dired-rsync-passphrase-stall-regex "Verification code")
   :bind (:map dired-mode-map ("C-c C-r" . dired-rsync)))
 
-(use-package dired-du)
-(use-package dired-sidebar
-  :bind (("C-c C-l" . dired-sidebar-toggle-sidebar))
-  :commands (dired-sidebar-toggle-sidebar))
-
 (use-package dirvish
   :ensure t
+  :bind
+  ("C-c C-l" . dirvish-side)
+  ("C-x C-d" . dirvish)
+  (:map dirvish-mode-map
+        ("TAB" . dirvish-subtree-toggle))
+
   :init
   ;; Let Dirvish take over Dired globally
   (dirvish-override-dired-mode))
