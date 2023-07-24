766b90b86fe1b2b4d9f26b452e9cfda1bb689d49
Adam Bobrow <adam@riseup.co.il>
2023-07-24 11:39:14 +0300

    fix cape

--- modules/completions.el
+++ modules/completions.el
@@ -51,7 +51,7 @@
   ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
   (add-to-list 'completion-at-point-functions #'cape-rfc1345)
   ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
-  (add-to-list 'completion-at-point-functions #'cape-ispell)
+  (add-to-list 'completion-at-point-functions #'cape-dict)
   ;; (add-to-list 'completion-at-point-functions #'cape-dict)
   ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
   ;;(add-to-list 'completion-at-point-functions #'cape-line))
