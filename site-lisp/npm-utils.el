47895459d4a523564f9faff21e2616c992fa3262
Adam Bobrow <adam@grainfinance.co>
2024-07-06 09:26:00 +0300

    Fix bob/toggle-update-node-modules-if-needed (WIP)

--- site-lisp/npm-utils.el
+++ site-lisp/npm-utils.el
@@ -14,27 +14,32 @@
     (mapcar 'string-to-number (string-split clean-version "\\."))))
 
 (defun bob/package-version-meet-requirements (required-version current-version)
-  (if (not (s-prefix? "^" required-version))
-    (equal current-version required-version)
-    (cl-destructuring-bind ((required-major required-minor required-patch)
-                            (current-major current-minor current-patch))
-        (list (version--breakdown required-version)
-              (version--breakdown current-version))
-      (and (equal required-major current-major)
-           (equal required-minor current-minor)
-           (>= current-minor required-minor)))))
+  (unless (if (not (s-prefix? "^" required-version))
+              (equal current-version required-version)
+            (cl-destructuring-bind ((required-major required-minor required-patch)
+                                    (current-major current-minor current-patch))
+                (list (version--breakdown required-version)
+                      (version--breakdown current-version))
+              (and (equal current-major required-major)
+                   (>= current-minor required-minor))))
+    (debug)))
 
 (ert-deftest version-requirements ()
-    (should (equal (bob/package-version-meet-requirements "^8.11.5" "8.11.5") t))
-    (should (equal (bob/package-version-meet-requirements "^8.11.5" "8.11.8") t))
-    (should (equal (bob/package-version-meet-requirements "^8.12.5" "8.11.5") nil))
-    (should (equal (bob/package-version-meet-requirements "^5.1.5" "6.1.5") nil)))
+  (should (equal (bob/package-version-meet-requirements "7.51.2" "7.117.0") t))
+  (should (equal (bob/package-version-meet-requirements "8.11.5" "8.11.5") t))
+  (should (equal (bob/package-version-meet-requirements "8.10.5" "8.11.5") nil))
+  (should (equal (bob/package-version-meet-requirements "7.10.5" "8.10.5") nil))
+  (should (equal (bob/package-version-meet-requirements "^8.11.5" "8.11.5") t))
+  (should (equal (bob/package-version-meet-requirements "^8.11.5" "8.11.8") t))
+  (should (equal (bob/package-version-meet-requirements "^8.12.5" "8.11.5") nil))
+  (should (equal (bob/package-version-meet-requirements "^5.1.5" "6.1.5") nil))
+  (should (equal (bob/package-version-meet-requirements "^3.100.0" "3.598.0") t)))
 
 (defun node--module-version (module-name)
   (assocdr 'version
            (bob/read-json-file (format "node_modules/%s/package.json" module-name))))
 
-(defun compare-version-to-module (name-to-version)
+(defun compare--version-to-module (name-to-version)
   (let* ((module (car name-to-version))
         (version (cdr name-to-version))
         (node-module-version (node--module-version module)))
@@ -44,22 +49,29 @@
   (let ((package-json-versions (bob/read-json-file "package.json")))
     (not (-every 'identity
                  (mapcar
-                  'compare-version-to-module
+                  'compare--version-to-module
                   (seq-concatenate 'list
                                    (assocdr 'dependencies package-json-versions)
                                    (assocdr 'devdependencies package-json-versions)))))))
 
+(defvar bob/update-node-modules-if-needed-on nil)
+(defun bob/toggle-update-node-modules-if-needed ()
+  (interactive)
+  (setq bob/update-node-modules-if-needed-on
+        (not bob/update-node-modules-if-needed-on)))
+
 ;;;###autoload
 (defun bob/update-node-modules-if-needed-sync ()
   (interactive)
-  (unwind-protect
-      (condition-case err
-          (when (and (time (bob/node-modules-are-not-updated-p))
-                     (y-or-n-p "node_modules are *not* up to date, Do you want to run `\"npm install\""))
-            ;; TODO allow using PNPM and bun as well
-            (npm-install-project nil t))
-        (error
-         (message "Error in npm-outdated-sentinel: %s." err)))))
+  (when bob/update-node-modules-if-needed-on
+    (unwind-protect
+        (condition-case err
+            (when (and (time (bob/node-modules-are-not-updated-p))
+                       (y-or-n-p "node_modules are *not* up to date, Do you want to run `\"npm install\""))
+              ;; TODO allow using PNPM and bun as well
+              (npm-install-project nil t))
+          (error
+           (message "Error in npm-outdated-sentinel: %s." err))))))
 
 ;; (ert-run-tests-batch 'version-requirements)
 (setq ert-quiet nil)
