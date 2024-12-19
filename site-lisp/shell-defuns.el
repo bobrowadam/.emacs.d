8a2475973ccf0c54f95a9f8af2ff0e4a8306362e
Adam Bobrow <adam@grainfinance.co>
2024-12-19 07:52:14 +0200

    [jest-ts] Fix completion candidate order

--- site-lisp/shell-defuns.el
+++ site-lisp/shell-defuns.el
@@ -76,7 +76,7 @@ Any other prefis will be used as the suffix itself."
                                 (equal (with-current-buffer b major-mode) 'eat-mode)
                                 (equal (with-current-buffer b major-mode) 'sql-interactive-mode)
                                 (equal (with-current-buffer b major-mode) 'roc-ts-repl-mode)
-                                (equal (with-current-buffer b major-mode) 'jest-test-compilation-mode)
+                                (equal (with-current-buffer b major-mode) 'jest-ts-mode/compilation-mode)
                                 ))
                 (mapcar (function buffer-name) (buffer-list))
                 ;; (flatten-tree (mapcar (λ (buffer-name %1)) (persp-get-buffers)))
