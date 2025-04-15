d591998b213245957b743428935d8f3ae4e2e508
Adam Bobrow <adam@grainfinance.co>
2025-04-15 21:01:08 +0300

    Use auto-commits=false in aider.el

--- modules/setup-llm.el
+++ modules/setup-llm.el
@@ -73,8 +73,9 @@ Be very aware of the tool API and the arguments it needs. failing to do so will 
 
 (use-package aider
   :ensure (:repo "tninja/aider.el" :fetcher github :files ("*.el"))
+  :custom
+  (aider-args '("--model" "sonnet" "--no-auto-accept-architect" "--auto-commits" "false"))
   :config
-  (setq aider-args '("--model" "sonnet" "--no-auto-accept-architect"))
   (setenv
    "ANTHROPIC_API_KEY"  (-some-> (auth-source-search :host "claude.ai" :max 1)
                           car
